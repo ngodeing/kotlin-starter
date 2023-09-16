81913ecbaeeffca6ac1862ff9d069c69f23461cb
ngodeing <onlytaufik47@gmail.com>
2023-09-16 18:35:57 +0700

    Melengkapi functional programming

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -227,6 +227,9 @@
 
 
 
+
+
+
 //FUNCTIONAL PROGRAMMING
 
 //fun main() {
@@ -270,3 +273,209 @@
 //fun sets(number: Array<Int>) {
 //
 //}
+
+
+
+//fun main() {
+//    10.printInt()
+//}
+//
+//fun Int.printInt() {
+//    print("value $this")
+//}
+
+
+
+//fun main() {
+//    println(10.slice)
+//}
+//
+//val Int.slice: Int
+//    get() = this / 2
+
+
+
+//fun main() {
+//    val value: Int? = null
+//
+//    println(value.slice)
+//}
+//
+//val Int?.slice: Int
+//    get() = this?.div(2) ?: 0
+
+
+
+//fun main() {
+//    val length = messageLength("Hello From lambda")
+//    println("Message length $length")
+//}
+//
+//val messageLength = { message: String -> message.length }
+
+
+
+//fun main() {
+//    printResult(10){ value ->
+//        value + value
+//    }
+//}
+//
+//fun printResult(value: Int, sum: (Int) -> Int) {
+//    val result = sum(value)
+//    println(result)
+//}
+
+
+
+//fun main() {
+//    val message = buildString {
+//        append("Hello ")
+//        append("from ")
+//        append("lambda ")
+//    }
+//
+//    println(message)
+//}
+//
+//fun buildString(action: StringBuilder.() -> Unit): String {
+//    val stringBuilder = StringBuilder()
+//    stringBuilder.action()
+//    return stringBuilder.toString()
+//}
+
+
+
+//fun main() {
+//    val text = "Hello"
+//    val result = text.run {
+//        val from = this
+//        val to = this.replace("Hello", "Kotlin")
+//        "replace text from $from to $to"
+//    }
+//    println("result : $result")
+//}
+
+//fun main() {
+//    val message = "Hello Kotlin!"
+//    val result = with(message) {
+//        println("value is $this")
+//        println("with length ${this.length}")
+//    }
+//}
+
+//fun main() {
+//    val message = StringBuilder().apply {
+//        append("Hello ")
+//        append("Kotlin!")
+//    }
+//
+//    println(message.toString())
+//}
+
+
+
+//fun main() {
+//    val message: String? = null
+//    message?.let {
+//        val length = it.length * 2
+//        val text = "text length $length"
+//        println(text)
+//    }
+//}
+
+
+//fun main(){
+//    print(sum(10,20,30))
+//}
+//fun sum(valueA: Int, valueB: Int, valueC: Int): Int {
+//    fun validateNumber(value: Int) {
+//        if (value == 0) {
+//            throw IllegalArgumentException("value must be better than 0")
+//        }
+//    }
+//
+//    validateNumber(valueA)
+//    validateNumber(valueB)
+//    validateNumber(valueC)
+//
+//    return valueA + valueB + valueC
+//}
+
+
+//fun main(){
+//    val numbers = listOf(1, 2, 3, 4, 5)
+//    val fold = numbers.fold(15) { current, item ->
+//        println("current $current")
+//        println("item $item")
+//        println()
+//        current + item
+//    }
+//
+//    println("Fold result: $fold")
+//}
+
+
+
+//fun main(){
+//    val number = listOf(1, 2, 3, 4, 5, 6)
+//    val drop = number.dropLast(3)
+//    val drop1 = number.drop(3)
+//
+//    val total = listOf(1, 2, 3, 4, 5, 6)
+//    val take = total.take(5)
+//
+//    println(take)
+//    println("^ Take  V Drop")
+//    println(drop)
+//    println(drop1)
+//}
+
+
+
+//fun main(){
+//    val total = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+//    val slice = total.slice(3..6)
+//
+//    println(slice)
+//
+//    val total1 = listOf(1, 2, 1, 3, 4, 5, 2, 3, 4, 5)
+//    val distinct = total1.distinct()
+//
+//    println(distinct)
+//
+//    val word = "QWERTY"
+//    val chunked = word.chunked(3)
+//
+//    println(chunked)
+//}
+
+
+
+//fun main() {
+//    println("Factorial 9999 is: ${factorial(9999)}")
+//}
+//
+//
+//fun factorial(n: Int): Int {
+//    return if (n == 1) {
+//        n
+//    } else {
+//        n * factorial(n - 1)
+//    }
+//}
+
+//fun main(){
+//    println(factorial(10,5))
+//}
+//tailrec fun factorial(n: Int, result: Int = 1): Int {
+//    val newResult = n * result
+//    return if (n == 1) {
+//        newResult
+//    } else {
+//        factorial(n - 1, newResult)
+//    }
+//}
+
+
+
